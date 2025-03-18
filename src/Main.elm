bf14e6be1624f37383c216c96519ae2ab6d5d8f9
Pontus Hjortskog <p.hjortskog@gmail.com>
2025-03-19 00:15:09 +0100

    Add tailwindcss

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,11 @@ module Main exposing (Msg(..), main, update, view)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
+main : Program () Int Msg
 main =
     Browser.sandbox { init = 0, update = update, view = view }
 
@@ -14,6 +16,7 @@ type Msg
     | Decrement
 
 
+update : Msg -> number -> number
 update msg model =
     case msg of
         Increment ->
@@ -23,9 +26,10 @@ update msg model =
             model - 1
 
 
+view : Int -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
+    div [ class "max-w-5xl m-auto text-center" ]
+        [ button [ onClick Decrement, class "px-4 py-2 border rounded-sm cursor-pointer" ] [ text "-" ]
         , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Increment, class "px-4 py-2 border rounded-sm cursor-pointer" ] [ text "+" ]
         ]

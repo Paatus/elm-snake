1ee31b5219ec1d4789c8ff06c3ef138364c37aad
Pontus Hjortskog <p.hjortskog@gmail.com>
2025-03-22 20:33:02 +0100

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import elmPlugin from 'vite-plugin-elm'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [elmPlugin(), tailwindcss()]
+  base: process.env.DEV ? '' : '/elm-snake/',
+  plugins: [elmPlugin(), tailwindcss()],
+  build: { outDir: "docs" }
 })
